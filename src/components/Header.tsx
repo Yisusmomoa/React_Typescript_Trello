34a6ca62d3d4497a3100db4c19cf1a3457bbda04
Brandon Loera Silva <44634476+Yisusmomoa@users.noreply.github.com>
2023-07-31 16:33:13 -0600

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,22 +14,26 @@ const Header = () => {
   }, [theme])
   
   return (
-    <header className='flex justify-around w-full p-3
+    <header className='flex justify-evenly w-full p-3 h-full
       border-solid border-2 border-sky-500 '>
-      <img src={logo} alt='Logo'/>
-      <div className='flex'>
-        <Link to={'/signin'}><h2>Iniciar sesión</h2></Link>
-        <Link to={'/signup'}><h2>Registro</h2></Link>
-      </div>
-      <div>
-        {
-          theme==='light' ? <HiMoon className='bg-slate-200 text-slate-800 
-            text-[35px] rounded-full p-1 cursor-pointer' onClick={toggleTheme} />
-          : <HiSun className='bg-slate-200 text-slate-800 
-            text-[35px] rounded-full p-1 cursor-pointer' onClick={toggleTheme} />
-        }
-        
-       
+      <Link to={'/'}>
+        <img src={logo} alt='Logo'/>
+      </Link>
+      <div className='flex justify-end w-2/3 gap-10 '>
+        <div className='flex gap-2'>
+          <Link to={'/signin'}> <h2 className='text-[22px] font-bold 
+            text-slate-900 dark:text-slate-300'>Iniciar sesión</h2> </Link>
+          <Link to={'/signup'}> <h2 className='text-[22px] font-bold 
+            text-slate-900 dark:text-slate-300'>Registro</h2> </Link>
+        </div>
+        <div>
+          {
+            theme==='light' ? <HiMoon className='bg-slate-200 text-slate-800 
+              text-[35px] rounded-full p-1 cursor-pointer' onClick={toggleTheme} />
+            : <HiSun className='bg-slate-200 text-slate-800 
+              text-[35px] rounded-full p-1 cursor-pointer' onClick={toggleTheme} />
+          }
+        </div>
       </div>
     </header>
   )
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -11,14 +11,25 @@ type ThemeContextProviderProps={
 }
 
 export const ThemeProvider:React.FC<ThemeContextProviderProps>=({children})=>{
+    
     const [theme, setTheme] = useState<Theme>((localStorage.getItem('theme'))as Theme || 'light')
     useEffect(() => {
         if(localStorage.getItem('theme')){
-            const storedTheme=(localStorage.getItem('theme')) as Theme
+            const storedTheme:Theme=(localStorage.getItem('theme')) as Theme || 'light'
             setTheme(storedTheme)
         }
     }, [])
-    
+    /*
+    as Theme: En TypeScript, as se utiliza para realizar una "Type Assertion" o "aserción de tipo". 
+    Indica al compilador que se espera que el valor retornado por localStorage.getItem('theme') sea del tipo Theme.
+    Es similar a un casteo en otros lenguajes.
+
+    || 'light': El operador de doble barra vertical || se denomina operador de coalescencia nula o nullish coalescing operator.
+    En este contexto, se utiliza como una expresión condicional.
+    Si el valor obtenido de localStorage.getItem('theme') es falsy (equivalente a null, undefined, 
+        false, 0, '', o NaN), entonces se asigna el valor 'light' a storedTheme.
+    Esto se conoce como "short-circuiting" o "cortocircuito".
+    */
     const data={theme, setTheme}
     return (
         <ThemeContext.Provider value={data} >
